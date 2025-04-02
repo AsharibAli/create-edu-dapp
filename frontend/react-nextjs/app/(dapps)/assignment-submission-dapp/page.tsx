31a6197dfefb54d3d0221883a82dcb74e336ea2b
AsharibAli <asharibarain4@gmail.com>
2025-04-02 11:31:29 +0500

    update

--- frontend/react-nextjs/app/(dapps)/assignment-submission-dapp/page.tsx
+++ frontend/react-nextjs/app/(dapps)/assignment-submission-dapp/page.tsx
@@ -37,16 +37,14 @@ export default function Component() {
   const [txnHash, setTxnHash] = useState<string | null>(null);
   const [showMessage, setShowMessage] = useState<boolean>(false);
   const [isEducator, setIsEducator] = useState<boolean>(false);
-  const [accountAddress, setAccountAddress] = useState<string | undefined>(
-    undefined
-  );
+  const [accountAddress, setAccountAddress] = useState<string | undefined>(undefined);
   const [isConnected, setIsConnected] = useState<boolean>(false);
 
   useEffect(() => {
     if (authState.idToken) {
       const decodedToken = jwtDecode<DecodedToken>(authState.idToken);
       setOcidUsername(decodedToken.edu_username);
-      setIsEducator(decodedToken.edu_username.startsWith("edu_"));
+      setIsEducator(decodedToken.edu_username.startsWith("asharib"));
     }
   }, [authState.idToken]);
 
@@ -57,7 +55,7 @@ export default function Component() {
       setAccountAddress(address);
       setIsConnected(true);
 
-      const contractAddress = "0x0EA845BCC2bafD0C54cD0CFfCEF23B57aac439ED";
+      const contractAddress = "0x2917CAaf110A3F3F89C2aC2DFFf4172A7eB13F27";
       const AssignmentSubmission = new web3Instance.eth.Contract(
         contractJson.abi,
         contractAddress
@@ -252,8 +250,7 @@ export default function Component() {
                       <a
                         className="text-teal-300"
                         href={
-                          "https://opencampus-codex.blockscout.com/tx/" +
-                          txnHash
+                          "https://educhain.blockscout.com/tx/" + txnHash
                         }
                         target="_blank"
                         rel="noopener noreferrer"
@@ -271,8 +268,8 @@ export default function Component() {
                 <AlertCircle className="h-4 w-4" />
                 <AlertTitle>Student View</AlertTitle>
                 <AlertDescription>
-                  As a student, you can submit assignments but cannot verify
-                  submissions. Only educators can verify submitted assignments.
+                  As a student, you can submit assignments but cannot verify submissions.
+                  Only educators can verify submitted assignments.
                 </AlertDescription>
               </Alert>
             )}
